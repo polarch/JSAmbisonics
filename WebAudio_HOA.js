0f2c1a396990b8805deec5ffb85c297d60f5a8f4
David Poirier-Quinot <davipoir@gmail.com>
2016-06-21 15:29:28 +0200

    implemented HRIR based decoding in HOA_binDecoder

--- WebAudio_HOA.js
+++ WebAudio_HOA.js
@@ -3,10 +3,10 @@
 //  archontis.politis@aalto.fi
 ////////////////////////////////////////////////////////////////////
 //
-//  WebAudio_HOA a JavaScript library for higher-order Ambisonics 
-//  The library implements Web Audio blocks that perform 
+//  WebAudio_HOA a JavaScript library for higher-order Ambisonics
+//  The library implements Web Audio blocks that perform
 //  typical ambisonic processing operations on audio signals.
-//  
+//
 ////////////////////////////////////////////////////////////////////
 
 /////////////////
@@ -15,7 +15,7 @@
 function HOA_encoder(audioCtx, order)
 {
     this.initialized = false;
-    
+
     this.ctx = audioCtx;
     this.order = order;
     this.nCh = (order+1)*(order+1);
@@ -39,16 +39,16 @@ function HOA_encoder(audioCtx, order)
         this.in.connect(this.gainNodes[i]);
         this.gainNodes[i].connect(this.out,0,i);
     }
-    
+
     this.initialized = true;
 }
-            
+
 HOA_encoder.prototype.updateGains = function()
 {
-    
+
     var N = this.order;
     var g_enc = computeRealSH(N, [[this.azi*Math.PI/180, this.elev*Math.PI/180]]);
-    
+
     for (var i=0; i<this.nCh; i++) {
         this.gains[i] = g_enc[i][0];
         this.gainNodes[i].gain.value = this.gains[i];
@@ -66,12 +66,12 @@ function HOA_orderLimiter(audioCtx, orderIn, orderOut)
     this.orderIn = orderIn;
     if (orderOut<orderIn) this.orderOut = orderOut;
     else this.orderOut = orderIn;
-    
+
     this.nChIn = (this.orderIn+1)*(this.orderIn+1);
     this.nChOut = (this.orderOut+1)*(this.orderOut+1);
     this.in = this.ctx.createChannelSplitter(this.nChIn);
     this.out = this.ctx.createChannelMerger(this.nChOut);
-    
+
     for (var i=0; i<this.nChOut; i++) this.in.connect(this.out, i, i);
 }
 
@@ -79,13 +79,13 @@ HOA_orderLimiter.prototype.updateOrder = function(orderOut)
 {
     if (orderOut<=this.orderIn) this.orderOut = orderOut;
     else return;
-    
+
     this.nChOut = (this.orderOut+1)*(this.orderOut+1);
     this.out.disconnect();
     this.out = this.ctx.createChannelMerger(this.nChOut);
-    
+
     for (var i=0; i<this.nChOut; i++) this.in.connect(this.out, i, i);
-    
+
 }
 
 /////////////////
@@ -103,24 +103,24 @@ function HOA_rotator(audioCtx, order)
     this.rotMtxNodes = new Array(this.order);
     this.in = null;
     this.out = null;
-    
+
     this.initialized = false;
-    
+
 }
 
 HOA_rotator.prototype.updateRotMtx = function()
 {
     if (!this.initialized) return;
-    
+
     var yaw = this.yaw*Math.PI/180;
     var pitch = this.pitch*Math.PI/180;
     var roll = this.roll*Math.PI/180;
 
     this.rotMtx = getSHrotMtx(yawPitchRoll2Rzyx(yaw, pitch, roll), this.order);
-    
+
     var band_idx = 1;
     for (var n=1; n<this.order+1; n++) {
-        
+
         for (var i=0; i<2*n+1; i++) {
             for (var j=0; j<2*n+1; j++) {
                 this.rotMtxNodes[n-1][i][j].gain.value = this.rotMtx[band_idx + i][band_idx + j];
@@ -133,14 +133,14 @@ HOA_rotator.prototype.updateRotMtx = function()
 HOA_rotator.prototype.init = function()
 {
     if (this.initialized) return;
-    
+
     // Input and output nodes
     this.in = this.ctx.createChannelSplitter(this.nCh);
     this.out = this.ctx.createChannelMerger(this.nCh);
-    
+
     // Initialize rotation gains to identity matrix
     for (var n=1; n<this.order+1; n++) {
-        
+
         var gains_n = new Array(2*n+1);
         for (var i=0; i<2*n+1; i++) {
             gains_n[i] = new Array(2*n+1);
@@ -152,10 +152,10 @@ HOA_rotator.prototype.init = function()
         }
         this.rotMtxNodes[n-1] = gains_n;
     }
-    
+
     // Create connections
     this.in.connect(this.out,0,0); // zeroth order ch. does not rotate
-    
+
     var band_idx = 1;
     for (var n=1; n<this.order+1; n++) {
         for (var i=0; i<2*n+1; i++) {
@@ -166,7 +166,7 @@ HOA_rotator.prototype.init = function()
         }
         band_idx = band_idx + 2*n+1;
     }
-    
+
     this.initialized = true;
 }
 
@@ -177,24 +177,17 @@ HOA_rotator.prototype.init = function()
 function HOA_binDecoder(audioCtx, order)
 {
     this.initialized = false;
-    
+
     this.ctx = audioCtx;
     this.order = order;
     this.nCh = (order+1)*(order+1);
-    this.decFilters = new Array(this.nCh);
-    this.decFilterNodes = new Array(this.nCh);
+    this.decFilters = new Array(2*this.nCh);
+    this.decFilterNodes = new Array(2*this.nCh);
     // input and output nodes
     this.in = this.ctx.createChannelSplitter(this.nCh);
     this.out = this.ctx.createChannelMerger(2);
-    // downmixing gains for left and right ears
-    this.gainMid = this.ctx.createGain();
-    this.gainSide = this.ctx.createGain();
-    this.invertSide = this.ctx.createGain();
-    this.gainMid.gain.value = 1;
-    this.gainSide.gain.value = 1;
-    this.invertSide.gain.value = -1;
     // convolver nodes
-    for (var i=0; i<this.nCh; i++) {
+    for (var i=0; i<2*this.nCh; i++) {
         this.decFilterNodes[i] = this.ctx.createConvolver();
         this.decFilterNodes[i].normalize = false;
     }
@@ -202,29 +195,24 @@ function HOA_binDecoder(audioCtx, order)
     this.resetFilters();
     // create audio connections
     for (var i=0; i<this.nCh; i++) {
-        this.in.connect(this.decFilterNodes[i],i,0);
-        var n = Math.floor(Math.sqrt(i));
-        var m = i-n*n-n;
-        if (m >=0) this.decFilterNodes[i].connect(this.gainMid);
-        else this.decFilterNodes[i].connect(this.gainSide);
+        // connect left ear filters
+        this.in.connect(this.decFilterNodes[2*i],i,0);
+        this.decFilterNodes[2*i].connect(this.out,0,0);
+        // connect right ear filters
+        this.in.connect(this.decFilterNodes[2*i+1],i,0);
+        this.decFilterNodes[2*i+1].connect(this.out,0,1);
     }
-    this.gainMid.connect(this.out,0,0);
-    this.gainSide.connect(this.out,0,0);
-    
-    this.gainMid.connect(this.out,0,1);
-    this.gainSide.connect(this.invertSide,0,0);
-    this.invertSide.connect(this.out,0,1);
-    
+
     this.initialized = true;
 }
 
 HOA_binDecoder.prototype.updateFilters = function(audioBuffer)
 {
     // assign filters to convolvers
-    for (var i=0; i<this.nCh; i++) {
+    for (var i=0; i<2*this.nCh; i++) {
         this.decFilters[i] = this.ctx.createBuffer(1, audioBuffer.length, audioBuffer.sampleRate);
         this.decFilters[i].getChannelData(0).set(audioBuffer.getChannelData(i));
-        
+
         this.decFilterNodes[i].buffer = this.decFilters[i];
     }
 }
@@ -243,14 +231,13 @@ HOA_binDecoder.prototype.resetFilters = function()
     }
 }
 
-
 /////////////////////////////////
 /* HOA VIRTUAL MICROPHONE */
 /////////////////////////////////
 function HOA_vmic(audioCtx, order)
 {
     this.initialized = false;
-    
+
     this.ctx = audioCtx;
     this.order = order;
     this.nCh = (order+1)*(order+1);
@@ -271,19 +258,19 @@ function HOA_vmic(audioCtx, order)
     this.SHxyz.fill(0);
     this.updatePattern();
     this.updateOrientation();
-    
+
     // Create connections
     for (i=0; i<this.nCh; i++) {
         this.in.connect(this.vmicGainNodes[i],i,0);
         this.vmicGainNodes[i].connect(this.out);
     }
-    
+
     this.initialized = true;
 }
 
 HOA_vmic.prototype.updatePattern = function()
 {
-    
+
     function computeCardioidCoeffs(N) {
         var coeffs = new Array(N+1);
         for (var n=0; n<N+1; n++) {
@@ -313,7 +300,7 @@ HOA_vmic.prototype.updatePattern = function()
         console.log(coeffs);
         return coeffs;
     }
-    
+
     switch(this.vmicPattern) {
         case "cardioid":
             // higher-order cardioid given by: (1/2)^N * ( 1+cos(theta) )^N
@@ -337,28 +324,28 @@ HOA_vmic.prototype.updatePattern = function()
             this.vmicPattern = "hypercardioid";
             this.vmicCoeffs = computeHypercardCoeffs(this.order);
     }
-    
+
     this.updateGains();
 }
 
 HOA_vmic.prototype.updateOrientation = function()
 {
-        
+
     var azi = this.azi*Math.PI/180;
     var elev = this.elev*Math.PI/180;
-    
+
     var tempSH = computeRealSH(this.order, [[azi, elev]]);
-    
+
     for (var i=1; i<this.nCh; i++) {
         this.SHxyz[i] = tempSH[i][0];
     }
-    
+
     this.updateGains();
 }
 
 HOA_vmic.prototype.updateGains = function()
 {
-        
+
     var q;
     for (var n=0; n<this.order+1; n++) {
         for (var m=-this.order; m<this.order+1; m++) {
@@ -366,7 +353,7 @@ HOA_vmic.prototype.updateGains = function()
             this.vmicGains[q] = (1/Math.sqrt(2*n+1)) * this.vmicCoeffs[n] * this.SHxyz[q];
         }
     }
-    
+
     for (var i=1; i<this.nCh; i++) {
         this.vmicGainNodes[i].gain.value = this.vmicGains[i];
     }
@@ -387,7 +374,7 @@ function HOA_bf2acn(audioCtx)
         this.gains[i] = this.ctx.createGain();
         if (i==0) this.gains[i].gain.value = Math.SQRT2;
         else this.gains[i].gain.value = Math.sqrt(3);
-        
+
         this.gains[i].connect(this.out,0,i);
     }
     this.in.connect(this.gains[0],0,0);
@@ -406,12 +393,12 @@ function HOA_acn2bf(audioCtx)
     this.in = this.ctx.createChannelSplitter(4);
     this.out = this.ctx.createChannelMerger(4);
     this.gains = [];
-    
+
     for (var i=0; i<4; i++) {
         this.gains[i] = this.ctx.createGain();
         if (i==0) this.gains[i].gain.value = Math.SQRT1_2;
         else this.gains[i].gain.value = 1/Math.sqrt(3);
-        
+
         this.gains[i].connect(this.out,0,i);
     }
     this.in.connect(this.gains[0],0,0);
